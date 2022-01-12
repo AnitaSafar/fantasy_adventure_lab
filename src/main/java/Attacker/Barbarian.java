fe0dfc5087c5ab18d8ebc27538bcbf16c4bcaf83
Anita Safar <ancsa0416@gmail.com>
2022-01-12 11:01:21 +0000

    hasName test done, Barbarian setup done

---
+++ src/main/java/Attacker/Barbarian.java
@@ -0,0 +1,12 @@
+package Attacker;
+
+import Player.Player;
+
+public class Barbarian extends Player {
+
+    public Barbarian(String name, int health) {
+        super(name, health);
+    }
+
+
+}
---
+++ src/test/java/BarbarianTest.java
@@ -0,0 +1,22 @@
+import Attacker.Barbarian;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BarbarianTest {
+
+    Barbarian barbarian;
+
+    @Before
+    public void before() {
+        barbarian = new Barbarian("Zanzibar", 1000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Zanzibar", barbarian.getName());
+    }
+
+
+}
