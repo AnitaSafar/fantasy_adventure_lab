66a611feaf301f54314c9c0d981da1fe2d6b40b4
Anita Safar <ancsa0416@gmail.com>
2022-01-12 11:13:07 +0000

    Dwarf created and tested

---
+++ src/main/java/Attacker/Dwarf.java
@@ -0,0 +1,12 @@
+package Attacker;
+
+import Player.Player;
+
+public class Dwarf extends Player {
+
+    public Dwarf(String name, int health) {
+        super(name, health);
+    }
+
+
+}
---
+++ src/test/java/DwarfTest.java
@@ -0,0 +1,37 @@
+import Attacker.Barbarian;
+import Attacker.Dwarf;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DwarfTest {
+    Dwarf dwarf;
+
+    @Before
+    public void before() {
+        dwarf = new Dwarf("Balfazaar", 1000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Balfazaar", dwarf.getName());
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(1000, dwarf.getHealth());
+    }
+
+    @Test
+    public void canSetHealth() {
+        dwarf.setHealth(900);
+        assertEquals(900, dwarf.getHealth());
+    }
+
+    @Test
+    public void canSetName() {
+        dwarf.setName("Pendragon");
+        assertEquals("Pendragon", dwarf.getName());
+    }
+}
